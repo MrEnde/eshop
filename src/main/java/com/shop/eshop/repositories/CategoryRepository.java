96a7e979c79a0195266fa9ba57feeb2af7d5d2e3
MrE <MrE554>
2021-07-26 17:26:51 +0300

    Add CategoryRepository

---
+++ src/main/java/com/shop/eshop/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.shop.eshop.repositories;
+
+import com.shop.eshop.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
+
