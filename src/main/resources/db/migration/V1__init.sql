fdfffd8db1933552091f16a6de9746e62965b5ca
MrE <MrE554>
2021-07-26 17:31:44 +0300

    Add table product and refactoring

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,34 +1,49 @@
+create table categories
+(
+    id         bigserial primary key not null,
+    title      varchar(255) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
 create table product
 (
     id bigserial not null
         constraint product_pk
             primary key,
-    name varchar not null,
-    price bigint not null
+    name varchar(255) not null,
+    price bigint not null,
+    category_id bigint references categories (id),
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
 );
 
 create unique index product_name_uindex
 	on product (name);
 
-insert into product (name, price, id)
+insert into categories (title, id)
     values
-           ('Milk', 85, 1),
-           ('Bread', 25, 2),
-           ('Cheese', 450, 3),
-           ('Coffee', 100, 4),
-           ('Snacks', 340, 5),
-           ('Porridge', 136, 6),
-           ('Cherry', 340, 7),
-           ('Meat', 570, 8),
-           ('Avocado', 360, 9),
-           ('Garlic', 12, 10),
-           ('Onion', 9, 11),
-           ('Almond', 89, 12),
-           ('Blackberry', 157, 13),
-           ('Lemon', 86, 14),
-           ('Lime', 150, 15),
-           ('Pecan', 273, 16),
-           ('Plum', 138, 17),
-           ('Walnut', 391, 18),
-           ('Barley', 2, 19),
-           ('Eggs', 50, 20);
+           ('Food', 1);
+
+insert into product (name, price, id, category_id)
+    values
+           ('Milk', 85, 1, 1),
+           ('Bread', 25, 2, 1),
+           ('Cheese', 450, 3, 1),
+           ('Coffee', 100, 4, 1),
+           ('Snacks', 340, 5, 1),
+           ('Porridge', 136, 6, 1),
+           ('Cherry', 340, 7, 1),
+           ('Meat', 570, 8, 1),
+           ('Avocado', 360, 9, 1),
+           ('Garlic', 12, 10, 1),
+           ('Onion', 9, 11, 1),
+           ('Almond', 89, 12, 1),
+           ('Blackberry', 157, 13, 1),
+           ('Lemon', 86, 14, 1),
+           ('Lime', 150, 15, 1),
+           ('Pecan', 273, 16, 1),
+           ('Plum', 138, 17, 1),
+           ('Walnut', 391, 18, 1),
+           ('Barley', 2, 19, 1),
+           ('Eggs', 50, 20, 1);
