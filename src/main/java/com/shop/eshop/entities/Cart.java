899509b7091f76eb1b7e6c3f51bedf6e9d09481a
MrE <MrE554>
2021-07-29 19:07:45 +0300

    Add Cart

---
+++ src/main/java/com/shop/eshop/entities/Cart.java
@@ -0,0 +1,80 @@
+package com.shop.eshop.entities;
+
+import com.shop.eshop.dto.OrderItemDto;
+import com.shop.eshop.models.Product;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@NoArgsConstructor
+@Data
+@Slf4j
+public class Cart {
+    private List<OrderItemDto> items;
+    private BigDecimal price;
+
+    @PostConstruct
+    public void init() {
+        this.items = new ArrayList<>();
+        this.price = BigDecimal.ZERO;
+    }
+
+    public void clear() {
+        items.clear();
+        price = BigDecimal.ZERO;
+    }
+
+    public boolean add(Long productId) {
+        for (OrderItemDto item : items) {
+            if (item.getProductId().equals(productId)) {
+                item.changeQuantity(1);
+                recalculate();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void add(Product product) {
+        items.add(new OrderItemDto(product));
+        recalculate();
+    }
+
+    public boolean delete(Long productId) {
+        for (OrderItemDto item : items) {
+            if (item.getProductId().equals(productId) && item.getQuantity() > 1) {
+                item.changeQuantity(-1);
+                recalculate();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void delete(Product product) {
+        int index = -1;
+        for (OrderItemDto item : items) {
+            if (item.getProductId().equals(product.getId())) {
+                index = items.indexOf(item);
+            }
+        }
+        if (index != -1) {
+            items.remove(index);
+        }
+        recalculate();
+    }
+
+    private void recalculate() {
+        price = BigDecimal.ZERO;
+        for (OrderItemDto item : items) {
+            price = price.add(item.getPrice());
+        }
+    }
+}
