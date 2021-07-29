31338917ddd7dbe024766581db1baa519dfe0dc7
MrE <MrE554>
2021-07-29 19:10:53 +0300

    Add OrderRepository

---
+++ src/main/java/com/shop/eshop/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.shop.eshop.repositories;
+
+import com.shop.eshop.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
