724841fb8207518db22003d01a7e24d3512fdf79
MrE <MrE554>
2021-07-29 19:16:33 +0300

    Add new feature

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,46 +1,85 @@
 angular.module('app', []).controller('indexController', function ($scope, $http) {
     const mainPath = 'http://localhost:8189/api/v1';
 
-    $scope.loadProducts = function () {
-        $http({
-            url: mainPath + '/products/',
-            method: 'GET',
-            params: {}
-        }).then(function (response) {
-            $scope.products = response.data;
-        });
-    };
-
     $scope.loadPage = function (pageIndex = 1) {
         $http({
-            url: mainPath + '/products/page',
+            url: mainPath + '/products',
             method: 'GET',
             params: {
                 'page': pageIndex
             }
         }).then(function (response) {
-            $scope.products = response.data.content;
+            $scope.productsPage = response.data;
+            $scope.navList = $scope.generatePagesIndexes(1, $scope.productsPage.totalPages);
+            console.log(response.data);
         });
     };
 
-    $scope.deleteProduct = function (productIndex) {
-        console.log($scope.products)
+    $scope.loadCart = function () {
         $http({
-            url: mainPath + '/products/delete/' + productIndex,
+            url: mainPath + '/cart',
             method: 'GET'
         }).then(function (response) {
-            let index = -1;
-
-            for (const product of $scope.products) {
-                if (product.id === productIndex) {
-                    index = $scope.products.indexOf(product)
-                }
-            }
-            if (index !== -1) {
-                $scope.products.splice(index, index + 1)
-            }
+            $scope.cart = response.data;
         });
     }
 
+    $scope.addToCart = function (productId) {
+        $http({
+            url: mainPath + '/cart/add/' + productId,
+            method: 'GET'
+        }).then(function (response) {
+            $scope.loadCart();
+        });
+    }
+
+    $scope.removeFromCart = function (productId) {
+        $http({
+            url: mainPath + '/cart/delete/' + productId,
+            method: 'DELETE'
+        }).then(function (response) {
+            $scope.loadCart();
+        })
+    }
+
+    $scope.clearCart = function () {
+        $http({
+            url: mainPath + '/cart/clear',
+            method: 'GET'
+        }).then(function (response) {
+            $scope.cart = null;
+        });
+    }
+
+    $scope.loadOrders = function () {
+        $http({
+            url: mainPath + '/orders',
+            method: 'GET'
+        }).then(function (response) {
+            $scope.orders = response.data;
+        });
+    }
+
+    $scope.createOrder = function () {
+        $http({
+            url: mainPath + '/orders',
+            method: 'POST'
+        }).then(function (response) {
+            alert('Заказ создан');
+            $scope.loadCart();
+            $scope.loadOrders();
+        });
+    }
+
+    $scope.generatePagesIndexes = function (startPage, endPage) {
+        let arr = [];
+        for (let i = startPage; i < endPage + 1; i++) {
+            arr.push(i);
+        }
+        return arr;
+    }
+
     $scope.loadPage();
+    $scope.loadCart();
+    $scope.loadOrders();
 });
